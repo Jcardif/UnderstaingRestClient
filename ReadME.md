63dc5aee0cf05a3eb7b4a7043f166e0ebb223f7d
Jcardif <josh.cardif14@outlook.com>
2018-03-17 00:23:31 +0300

    Retrieve string

---
+++ ReadME.md
@@ -0,0 +1,5 @@
+# Understanding Rest Client In C#
+##### Dependencies
+Install the following nuget Packages in your .NET console application.
+<li>System.Runtime.Serailization.Json
+<li>Microsoft.AspNet.WebApi.Client
--- UnderstaingRestClient/Program.cs
+++ UnderstaingRestClient/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
 
 namespace UnderstaingRestClient
 {
     class Program
     {
+        private static readonly HttpClient Client=new HttpClient();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ProcessRepositories().Wait();
+        }
+
+        private static async Task ProcessRepositories()
+        {
+            Client.DefaultRequestHeaders.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            Client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            var streamTask = Client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var serilizer = new DataContractJsonSerializer(typeof(List<Repo>));
+            Console.WriteLine(message);
         }
     }
 }
