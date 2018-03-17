39b70b52c8ec2bc7dfdfa513d40c3142229fe812
Jcardif <josh.cardif14@outlook.com>
2018-03-17 06:32:28 +0300

    Started rest client

--- UnderstaingRestClient/RestClient.cs
+++ UnderstaingRestClient/RestClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,23 @@ namespace UnderstandingRestClient
     internal class RestClient
     {
         private static readonly HttpClient Client=new HttpClient();
-        private static void Main(string[] args)
+        private static void Main()
         {
             RunAsync().GetAwaiter().GetResult();
         }
-        static void ShowProduct(List<Product> productlist)
+        static void ShowProductList(List<Product> productlist)
         {
-            Console.WriteLine("Product Name\tPrice\tCategory");
+            Console.Clear();
             foreach (var product in productlist)
             {
-                Console.WriteLine($"{product.Name}\t{product.Price}\t{product.Category}");
+                Console.WriteLine($"Id : {product.Id}\nName : {product.Name}\nPrice : {product.Price}\n{product.Category}\n\n");
             }
+        }
+
+        static void ShowProduct(Product product)
+        {
+            Console.Clear();
+            Console.WriteLine($"Id : {product.Id}\nName : {product.Name}\nPrice : {product.Price}\n{product.Category}");
         }
 
         static async Task RunAsync()
@@ -30,10 +37,39 @@ namespace UnderstandingRestClient
             Client.BaseAddress=new Uri("http://localhost:60154");
             Client.DefaultRequestHeaders.Accept.Clear();
             Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/Json"));
+
             try
             {
-               var product = await GetProductAsync();
-                ShowProduct(product);
+                Console.WriteLine("1. Get All\n2. Get With Id\n3. Create an Object");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Enter the Id of the product");
+                        ShowProduct(await GetProductAsync(Convert.ToInt32(Console.ReadLine())));
+                        Console.ReadKey();
+                        Main();
+                        break;
+                    case 2:
+                        ShowProductList(await GetProductListAsync());
+                        Console.ReadKey();
+                        Main();
+                        break;
+                    case 3:
+                        Product prdt = new Product()
+                        {
+                            Name = "Casio690",
+                            Price = 2300,
+                            Category = "Calculators"
+                        };
+                        //Console.Clear();
+                        Console.WriteLine($"{prdt.Name} has been created at {await CreateProductAsync(prdt)}");
+                        Main();
+                        break;
+                    default:
+                        Console.WriteLine("Choose a value between 1 and 4");
+                        break;
+                }
             }
             catch (Exception e)
             {
@@ -42,7 +78,7 @@ namespace UnderstandingRestClient
             }
         }
 
-        private static async Task<List<Product>> GetProductAsync()
+        private static async Task<List<Product>> GetProductListAsync()
         {
             List<Product>lstproduct = new List<Product>();
             HttpResponseMessage response = await Client.GetAsync("api/Product");
@@ -51,6 +87,30 @@ namespace UnderstandingRestClient
                 lstproduct = JsonConvert.DeserializeObject<List<Product>>(await response.Content.ReadAsStringAsync());
             }
             return lstproduct;
+        }
+
+        private static async Task<Product> GetProductAsync(int id)
+        {
+            Product product=new Product();
+            HttpResponseMessage response = await Client.GetAsync($"api/Product/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                product = await response.Content.ReadAsAsync<Product>();
+            }
+            return product;
+        }
+
+        private static async Task<Uri> CreateProductAsync(Product product)
+        {
+            Product prdt = new Product()
+            {
+                Name = "Casio690",
+                Price = 2300,
+                Category = "Calculators"
+            };
+            HttpResponseMessage response = await Client.PostAsync("api/Product", prdt,new JsonMediaTypeFormatter());
+            response.EnsureSuccessStatusCode();
+            return response.Headers.Location;
         }
     }
 }
