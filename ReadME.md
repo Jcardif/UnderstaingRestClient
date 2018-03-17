8bc1e031b2e0c3f1d020f5ac6829cd9e635c4647
Jcardif <josh.cardif14@outlook.com>
2018-03-17 05:02:53 +0300

    Get alll

--- ReadME.md
+++ ReadME.md
@@ -3,9 +3,36 @@ A simple console application demonstrating how to build a Rest Client in C#
 #### HTTP Client
 <p>HttpClient is a modern HTTP client for .NET. It provides a flexible and extensible API for accessing all things exposed through HTTP. 
 
-##### Dependencies
+#### Setting the Web-API(Rest Service)
+Download the [Database File](https://github.com/Jcardif/UnderstaingRestClient/blob/master/Database/productdb.sql) to create your MySQL database.
+
+Create an Empty WebApi Project and include it in your current solution
+Next Create a class named Product.cs  under the model folder and define the class as follows:
+```CSharp
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+    }
+```   
+Next Create a class to handle the controller's operations and subsequently create a controller.  
+
+Click [here](https://github.com/Jcardif/RestService) for more help on how create a rest service
+
+#### Dependencies
 Install the following nuget Packages in your .NET console application.
 <li>System.Runtime.Serialization.Json
 <li>Microsoft.AspNet.WebApi.Client
 <li>NewtonSoft.Json
+
+Add a reference to your rest service project
+
+#### Building your Rest Client
+Initialize your Rest Client as follows
+```CSharp
+ private static readonly HttpClient Client=new HttpClient();
+```
+
 
---
+++ UnderstaingRestClient/RestClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestService.Models;
+
+namespace UnderstandingRestClient
+{
+    internal class RestClient
+    {
+        private static readonly HttpClient Client=new HttpClient();
+        private static void Main(string[] args)
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+        static void ShowProduct(List<Product> productlist)
+        {
+            Console.WriteLine("Product Name\tPrice\tCategory");
+            foreach (var product in productlist)
+            {
+                Console.WriteLine($"{product.Name}\t{product.Price}\t{product.Category}");
+            }
+        }
+
+        static async Task RunAsync()
+        {
+            Client.BaseAddress=new Uri("http://localhost:60154");
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/Json"));
+            try
+            {
+               var product = await GetProductAsync();
+                ShowProduct(product);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private static async Task<List<Product>> GetProductAsync()
+        {
+            List<Product>lstproduct = new List<Product>();
+            HttpResponseMessage response = await Client.GetAsync("api/Product");
+            if (response.IsSuccessStatusCode)
+            {
+                lstproduct = JsonConvert.DeserializeObject<List<Product>>(await response.Content.ReadAsStringAsync());
+            }
+            return lstproduct;
+        }
+    }
+}
